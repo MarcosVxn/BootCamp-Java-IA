c227bbdbf08488c3cd093e2b5958736c4e12071a
MarcosVxn <marcosjoaomj76@gmail.com>
2026-08-31 17:30:25 -0300

    Feat: Atividade-01-Fundamentos

--- exercicios-java/exercicios/Exercicio_1/CalculoIdade.java
+++ exercicios-java/exercicios/Exercicio_1/CalculoIdade.java
@@ -1,5 +1,23 @@
 package Exercicio_1;
 
+import java.util.Scanner;
+
 public class CalculoIdade {
+
+    private static int ANOATUAL = 2026;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Qual o seu nome? ");
+        String nome = scan.next();
+        System.out.print("Em que ano você nasceu? ");
+        int dataNasc = scan.nextInt();
+
+        int idade = ANOATUAL - dataNasc;
+
+        System.out.println("Ola " + nome + "Você tem " + idade + " anos");
+
+
+        scan.close();
+    }
     
 }
